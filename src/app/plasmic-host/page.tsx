b64590165827d8d36094609f5c3c887273e4bdf3
sambajarju2-png <sambajarju2@gmail.com>
2026-03-17 23:34:30 +0100

    Fix: Plasmic host client-only rendering

--- src/app/plasmic-host/page.tsx
+++ src/app/plasmic-host/page.tsx
@@ -1,5 +1,12 @@
-import { PlasmicCanvasHost } from '@plasmicapp/loader-nextjs';
+'use client';
+
+import dynamic from 'next/dynamic';
 import '@/lib/plasmic';
+
+const PlasmicCanvasHost = dynamic(
+  () => import('@plasmicapp/loader-nextjs').then((mod) => mod.PlasmicCanvasHost),
+  { ssr: false }
+);
 
 export default function PlasmicHostPage() {
   return <PlasmicCanvasHost />;
